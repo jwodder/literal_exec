4f13fb3b38d099393ef5b5861f13c69e97f22be3
John T. Wodder II <git@varonathe.org>
2017-05-25 07:52:46 +0000

    Handle multiple assignments

--- literal_exec.py
+++ literal_exec.py
@@ -17,7 +17,7 @@ def literal_exec(src):
     for statement in top_level.body:
         if not isinstance(statement, ast.Assign):
             continue
-        assert len(statement.targets) == 1
-        name = statement.targets[0].id
-        result[name] = ast.literal_eval(statement.value)
+        value = ast.literal_eval(statement.value)
+        for target in statement.targets:
+            result[target.id] = value
     return result
